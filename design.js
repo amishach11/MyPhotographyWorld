a9e2bc0a6d97b7aa1256b2ccbb281e25844a2046
amisha <amishachopra21@gmail.com>
2021-07-16 12:08:30 +0530

    my first commit

---
+++ design.js
@@ -0,0 +1,42 @@
+
+
+var love1 = document.querySelector(".heart1");
+ love1.addEventListener('click',color);
+
+
+var love2 = document.querySelector(".heart2");
+ love2.addEventListener('click',color);
+
+
+
+var love3 = document.querySelector(".heart3");
+ love3.addEventListener('click',color);
+
+
+var love4 = document.querySelector(".heart4");
+ love4.addEventListener('click',color);
+
+
+var love5 = document.querySelector(".heart5");
+ love5.addEventListener('click',color);
+
+
+var love6 = document.querySelector(".heart6");
+ love6.addEventListener('click',color);
+
+function color(){
+	love.style.color="red";
+	console.log("clicked");
+}
+
+
+
+
+// var love=document.getElementByClass('heart');
+// love.addEventListener('click',c);
+// var ans=0;
+// function counter(){
+//   ans++;
+//   var para=document.getElementById('count');
+//   para.innerHTML = ans;
+// }
